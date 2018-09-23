cefbf45bfaf2182d20e7ed57beaa0545b6413999
James Hook <jameshook@Jamess-MacBook-Pro.local>
2018-09-23 18:58:10 +0100

    runs a manual player only game from swift

--- c4main/Sources/c4main/main.swift
+++ c4main/Sources/c4main/main.swift
@@ -3,3 +3,8 @@ import conn4
 
 // print("Hello, world!")
 print(conn4.text)
+let p1 = ManualPlayer(colour: 1)
+let p2 = ManualPlayer(colour: 2)
+let g = Connect4Game( p1: p1  ,p2:  p2 )
+print( "---")
+g.start_game()
